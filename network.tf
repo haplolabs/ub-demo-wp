e24fd87f62f72571d3dfe5959ebb59d0dba11c90
Nate Thompson <haplo@haplolabs.io>
2021-05-30 14:33:19 +0000

    Updated firewall config

--- network.tf
+++ network.tf
@@ -100,11 +100,20 @@ resource "digitalocean_record" "public" {
     ttl    = 300
 }
 
+################################################################################
+# Firewall Rules for our Webserver Droplets                                    #
+################################################################################
 resource "digitalocean_firewall" "web" {
-  name = "only-22-80-and-443"
 
+  # The name we give our firewall for ease of use                            #    
+  name = "minimal-vpc-only-vpc-traffic"
+
+  # The droplets to apply this firewall to                                   #
   droplet_ids = digitalocean_droplet.web.*.id
 
+  #--------------------------------------------------------------------------#
+  # Internal VPC Rules. We have to let ourselves talk to each other          #
+  #--------------------------------------------------------------------------#
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
@@ -112,46 +121,65 @@ resource "digitalocean_firewall" "web" {
   }
 
   inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "tcp"
+    port_range = "1-65535"
+    source_addresses = [digitalocean_vpc.web.ip_range]
   }
 
   inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "udp"
+    port_range = "1-65535"
+    source_addresses = [digitalocean_vpc.web.ip_range]
   }
 
   inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "icmp"
+    source_addresses = [digitalocean_vpc.web.ip_range]
   }
 
   outbound_rule {
-    protocol              = "tcp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "udp"
+    port_range = "1-65535"
+    destination_addresses = [digitalocean_vpc.web.ip_range]
   }
 
   outbound_rule {
-    protocol              = "udp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "tcp"
+    port_range = "1-65535"
+    destination_addresses = [digitalocean_vpc.web.ip_range]
   }
 
   outbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
+    protocol = "icmp"
+    destination_addresses = [digitalocean_vpc.web.ip_range]
   }
 
+  #--------------------------------------------------------------------------#
+  # Selective Outbound Traffic Rules                                         #
+  #--------------------------------------------------------------------------#
+
+  # DNS
   outbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
+    protocol = "udp"
+    port_range = "53"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  # HTTP
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # HTTPS
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "443"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # ICMP (Ping)
   outbound_rule {
     protocol              = "icmp"
     destination_addresses = ["0.0.0.0/0", "::/0"]
